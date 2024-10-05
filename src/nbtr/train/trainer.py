19b9505acb73c97756070cbb3cd10fb4b904bc0d
habanoz <huseyinabanox@gmail.com>
2024-10-05 11:28:12 +0300

    update mfu calculations.

--- src/nbtr/train/trainer.py
+++ src/nbtr/train/trainer.py
@@ -311,7 +311,7 @@ class Trainer:
                                 running_fwd_bwd_tokens_per_sec = 0.9*running_fwd_bwd_tokens_per_sec + 0.1*fwd_bwd_tokens_per_sec
                                 running_iter_time = 0.9* running_iter_time + 0.1 * iter_time
                             
-                            mfu = estimate_mfu(model=raw_model,fwdbwd_per_iter=self.config.batch_size * self.config.gradient_accumulation_steps * self.world_size, flops_promised=self.config.promised_flops,dt=iter_time)
+                            mfu = estimate_mfu(model=raw_model,fwdbwd_per_iter=self.config.batch_size * self.config.gradient_accumulation_steps, flops_promised=self.config.promised_flops,dt=iter_time)
                             
                         print(f"iter {it}: loss {loss_sum:.4f}, iter_time {iter_time*1000:.2f}ms, run_iter_time {running_iter_time*1000:.2f}ms, fb_toks/sec {fwd_bwd_tokens_per_sec:.2f}, run_fb_toks/sec {running_fwd_bwd_tokens_per_sec:.2f}, mfu {mfu:.3f}")
 
