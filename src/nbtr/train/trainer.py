ccef6812d641413c9b08df604f4d6750417b0466
habanoz <huseyinabanox@gmail.com>
2024-10-25 08:11:30 +0300

    fix lr reporting at the beginning..

--- src/nbtr/train/trainer.py
+++ src/nbtr/train/trainer.py
@@ -263,7 +263,7 @@ class Trainer:
                 self.skip_first_new_best_val_loss = False
             elif self.rank == 0:
                 # do not evaluate before-hand if resuming...
-                self.do_eval(raw_model, optimizer, running_fwd_bwd_tokens_per_sec, running_iter_time, start_iter, self.config.learning_rate, wb_run, 0.0)
+                self.do_eval(raw_model, optimizer, running_fwd_bwd_tokens_per_sec, running_iter_time, start_iter, self.get_lr(0), wb_run, 0.0)
             
             for it in range(start_iter, self.config.max_iters+1):
                 
