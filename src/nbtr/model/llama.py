d8387669e4fd3af41383dfc72070c4c2a2dff1d6
habanoz <huseyinabanox@gmail.com>
2024-10-27 21:43:04 +0300

    optimized rope

--- src/nbtr/model/llama.py
+++ src/nbtr/model/llama.py
@@ -5,6 +5,7 @@ from torch.nn import functional as F
 import math
 from torch.utils.checkpoint import checkpoint as gc
 import torch.version
+from typing import Tuple
 
 @dataclass
 class GPTConfig:
@@ -24,66 +25,60 @@ class GPTConfig:
         
         return GPTConfig(**doc)
 
-def rotate_half(x):
-    """Rotates half the hidden dims of the input."""
-    x1 = x[..., : x.shape[-1] // 2]
-    x2 = x[..., x.shape[-1] // 2 :]
-    return torch.cat((-x2, x1), dim=-1)
 
+def apply_scaling(freqs: torch.Tensor):
+    # Values obtained from grid search
+    scale_factor = 8
+    low_freq_factor = 1
+    high_freq_factor = 4
+    old_context_len = 8192  # original llama3 length
 
-def apply_rotary_pos_emb(q, k, cos, sin, position_ids=None, unsqueeze_dim=1):
-    """Applies Rotary Position Embedding to the query and key tensors.
+    low_freq_wavelen = old_context_len / low_freq_factor
+    high_freq_wavelen = old_context_len / high_freq_factor
+    new_freqs = []
+    for freq in freqs:
+        wavelen = 2 * math.pi / freq
+        if wavelen < high_freq_wavelen:
+            new_freqs.append(freq)
+        elif wavelen > low_freq_wavelen:
+            new_freqs.append(freq / scale_factor)
+        else:
+            assert low_freq_wavelen != high_freq_wavelen
+            smooth = (old_context_len / wavelen - low_freq_factor) / (
+                high_freq_factor - low_freq_factor
+            )
+            new_freqs.append((1 - smooth) * freq / scale_factor + smooth * freq)
+    return torch.tensor(new_freqs, dtype=freqs.dtype, device=freqs.device)
 
-    Args:
-        q (`torch.Tensor`): The query tensor.
-        k (`torch.Tensor`): The key tensor.
-        cos (`torch.Tensor`): The cosine part of the rotary embedding.
-        sin (`torch.Tensor`): The sine part of the rotary embedding.
-        position_ids (`torch.Tensor`, *optional*):
-            Deprecated and unused.
-        unsqueeze_dim (`int`, *optional*, defaults to 1):
-            The 'unsqueeze_dim' argument specifies the dimension along which to unsqueeze cos[position_ids] and
-            sin[position_ids] so that they can be properly broadcasted to the dimensions of q and k. For example, note
-            that cos[position_ids] and sin[position_ids] have the shape [batch_size, seq_len, head_dim]. Then, if q and
-            k have the shape [batch_size, heads, seq_len, head_dim], then setting unsqueeze_dim=1 makes
-            cos[position_ids] and sin[position_ids] broadcastable to the shapes of q and k. Similarly, if q and k have
-            the shape [batch_size, seq_len, heads, head_dim], then set unsqueeze_dim=2.
-    Returns:
-        `tuple(torch.Tensor)` comprising of the query and key tensors rotated using the Rotary Position Embedding.
-    """
-    cos = cos.unsqueeze(unsqueeze_dim)
-    sin = sin.unsqueeze(unsqueeze_dim)
-    q_embed = (q * cos) + (rotate_half(q) * sin)
-    k_embed = (k * cos) + (rotate_half(k) * sin)
-    return q_embed, k_embed
+def precompute_freqs_cis(
+    dim: int, end: int, theta: float = 10000.0, use_scaled: bool = False
+):
+    freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
+    t = torch.arange(end, device=freqs.device, dtype=torch.float32)
+    if use_scaled:
+        freqs = apply_scaling(freqs)
+    freqs = torch.outer(t, freqs)
+    freqs_cis = torch.polar(torch.ones_like(freqs), freqs)  # complex64
+    return freqs_cis
 
-class LlamaRotaryEmbedding(nn.Module):
-    def __init__(self, dim, max_position_embeddings=2048, base=10000, device=None, scaling_factor=1.0):
-        super().__init__()
-        self.scaling_factor = scaling_factor
-        self.dim = dim
-        self.max_position_embeddings = max_position_embeddings
-        self.base = base
-        inv_freq = 1.0 / (self.base ** (torch.arange(0, self.dim, 2, dtype=torch.int64).float().to(device) / self.dim))
-        self.register_buffer("inv_freq", inv_freq, persistent=False)
-        # For BC we register cos and sin cached
-        self.max_seq_len_cached = max_position_embeddings
+def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor):
+    ndim = x.ndim
+    assert 0 <= 1 < ndim
+    assert freqs_cis.shape == (x.shape[1], x.shape[-1])
+    shape = [d if i == 1 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)]
+    return freqs_cis.view(*shape)
 
-    @torch.no_grad()
-    def forward(self, x, position_ids):
-        # x: [bs, num_attention_heads, seq_len, head_size]
-        inv_freq_expanded = self.inv_freq[None, :, None].float().expand(position_ids.shape[0], -1, 1)
-        position_ids_expanded = position_ids[:, None, :].float()
-        # Force float32 since bfloat16 loses precision on long contexts
-        # See https://github.com/huggingface/transformers/pull/29285
-        device_type = x.device.type
-        device_type = device_type if isinstance(device_type, str) and device_type != "mps" else "cpu"
-        with torch.autocast(device_type=device_type, enabled=False):
-            freqs = (inv_freq_expanded.float() @ position_ids_expanded.float()).transpose(1, 2)
-            emb = torch.cat((freqs, freqs), dim=-1)
-            cos = emb.cos()
-            sin = emb.sin()
-        return cos.to(dtype=x.dtype), sin.to(dtype=x.dtype)
+def apply_rotary_emb(
+    xq: torch.Tensor,
+    xk: torch.Tensor,
+    freqs_cis: torch.Tensor,
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    xq_ = torch.view_as_complex(xq.float().reshape(*xq.shape[:-1], -1, 2))
+    xk_ = torch.view_as_complex(xk.float().reshape(*xk.shape[:-1], -1, 2))
+    freqs_cis = reshape_for_broadcast(freqs_cis, xq_)
+    xq_out = torch.view_as_real(xq_ * freqs_cis).flatten(3)
+    xk_out = torch.view_as_real(xk_ * freqs_cis).flatten(3)
+    return xq_out.type_as(xq), xk_out.type_as(xk)
     
 class MLP(nn.Module):
     def __init__(self, config) -> None:
@@ -113,12 +108,6 @@ class CausalSelfAttention(nn.Module):
         # output projection
         self.c_proj = nn.Linear(config.n_embed, config.n_embed, bias=False)
         
-        self.rotary_emb = LlamaRotaryEmbedding(
-                self.head_dim,
-                max_position_embeddings=config.seq_length,
-                base=10000.0 #self.rope_theta,
-            )
-        
         # regularization
         self.attn_dropout = nn.Dropout(config.dropout)
         self.resid_dropout = nn.Dropout(config.dropout)
@@ -133,7 +122,7 @@ class CausalSelfAttention(nn.Module):
             self.register_buffer("bias", torch.tril(torch.ones(config.seq_length, config.seq_length))
                                         .view(1, 1, config.seq_length, config.seq_length))
 
-    def forward(self, x, position_ids):
+    def forward(self, x, freqs_cis: torch.Tensor):
         B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embed)
 
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
@@ -143,8 +132,7 @@ class CausalSelfAttention(nn.Module):
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         
         # rope
-        cos, sin = self.rotary_emb(v, position_ids)
-        q, k = apply_rotary_pos_emb(q, k, cos, sin)
+        q, k = apply_rotary_emb(q, k, freqs_cis=freqs_cis)
 
         # gqa
         # key_states = repeat_kv(key_states, self.num_key_value_groups) # (B, num_attention_heads, T, hs)
@@ -176,14 +164,14 @@ class Block(nn.Module):
         self.ln_2 = nn.LayerNorm(config.n_embed, bias=False)
         self.mlp = MLP(config)
 
-    def forward(self, x, position_ids):
-        x = x + self.attn(self.ln_1(x), position_ids)
+    def forward(self, x, freqs_cis):
+        x = x + self.attn(self.ln_1(x), freqs_cis)
         x = x + self.mlp(self.ln_2(x))
         return x
 
 
 class GPT(nn.Module):
-    def __init__(self, config):
+    def __init__(self, config:GPTConfig):
         super().__init__()
 
         # version check, otherwise make sure q,k,v tensors are continuous
@@ -207,6 +195,13 @@ class GPT(nn.Module):
  
         # weight-tying
         self.transformer.wte.weight = self.lm_head.weight
+        
+        self.freqs_cis = precompute_freqs_cis(
+            config.n_embed // config.n_head,
+            config.seq_length * 2,
+            5e+5, # config.rope_theta,
+            False #config.use_scaled_rope,
+        )
 
         # init all weights
         self.apply(self._init_weights)
@@ -227,20 +222,21 @@ class GPT(nn.Module):
     def gradient_checkpointing(self, value:bool):
         self._gradient_checkpointing = value
     
-    def forward(self, idx, targets=None):
+    def forward(self, idx, targets=None, ignore_index=-100):
         device = idx.device
         B, T = idx.size()
         assert T <= self.config.seq_length, f"Input sequence length ({T}) cannot be larger than model sequence lenfgth {self.config.seq_length} "
 
-        position_ids = torch.arange(0, T, dtype=torch.long, device=device).unsqueeze(0)
+        # self.freqs_cis = self.freqs_cis.to(device)
+        freqs_cis = self.freqs_cis[:T]
         
         x = self.transformer.wte(idx)
         
         for block in self.transformer.h:
             if self._gradient_checkpointing and self.training:
-                x = gc(block,x, position_ids, use_reentrant=True)
+                x = gc(block,x, freqs_cis, use_reentrant=True)
             else:
-                x = block(x, position_ids)
+                x = block(x, freqs_cis)
             
         x = self.transformer.ln_f(x)
         
