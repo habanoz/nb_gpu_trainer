a6ca3d527d5467cc47aadba39175b70fd7f2f97f
habanoz <huseyinabanox@gmail.com>
2024-10-29 09:20:31 +0300

    fix transpose

--- src/nbtr/model/llama.py
+++ src/nbtr/model/llama.py
@@ -50,6 +50,7 @@ def apply_scaling(freqs: torch.Tensor):
             new_freqs.append((1 - smooth) * freq / scale_factor + smooth * freq)
     return torch.tensor(new_freqs, dtype=freqs.dtype, device=freqs.device)
 
+
 def precompute_freqs_cis(
     dim: int, end: int, theta: float = 10000.0, use_scaled: bool = False
 ):
@@ -61,12 +62,14 @@ def precompute_freqs_cis(
     freqs_cis = torch.polar(torch.ones_like(freqs), freqs)  # complex64
     return freqs_cis
 
+
 def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor):
     ndim = x.ndim
     assert 0 <= 1 < ndim
-    assert freqs_cis.shape == (x.shape[1], x.shape[-1]), f"{freqs_cis.shape} != {(x.shape[1], x.shape[-1])}"
+    assert freqs_cis.shape == (x.shape[1], x.shape[-1])
     shape = [d if i == 1 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)]
     return freqs_cis.view(*shape)
+
 
 def apply_rotary_emb(
     xq: torch.Tensor,
@@ -76,8 +79,8 @@ def apply_rotary_emb(
     xq_ = torch.view_as_complex(xq.float().reshape(*xq.shape[:-1], -1, 2))
     xk_ = torch.view_as_complex(xk.float().reshape(*xk.shape[:-1], -1, 2))
     freqs_cis = reshape_for_broadcast(freqs_cis, xq_)
-    xq_out = torch.view_as_real(xq_ * freqs_cis).clone().flatten(3)
-    xk_out = torch.view_as_real(xk_ * freqs_cis).clone().flatten(3)
+    xq_out = torch.view_as_real(xq_ * freqs_cis).flatten(3)
+    xk_out = torch.view_as_real(xk_ * freqs_cis).flatten(3)
     return xq_out.type_as(xq), xk_out.type_as(xk)
     
 class MLP(nn.Module):
@@ -134,6 +137,10 @@ class CausalSelfAttention(nn.Module):
         # rope
         q, k = apply_rotary_emb(q, k, freqs_cis=freqs_cis)
 
+        k = k.transpose(1,2)
+        q = q.transpose(1,2)
+        v = v.transpose(1,2)
+        
         # gqa
         # key_states = repeat_kv(key_states, self.num_key_value_groups) # (B, num_attention_heads, T, hs)
         # value_states = repeat_kv(value_states, self.num_key_value_groups) # (B, num_attention_heads, T, hs)
@@ -199,11 +206,13 @@ class GPT(nn.Module):
         freqs_cis = precompute_freqs_cis(
             config.n_embed // config.n_head,
             config.seq_length * 2,
-            5e+5, # config.rope_theta,
+            1e+4, # config.rope_theta,
             False #config.use_scaled_rope,
         )
 
-        self.register_buffer("freqs_cis", freqs_cis)
+        assert freqs_cis.requires_grad==False,"Precomputed cis tensor cannot accumulate grad"
+        assert freqs_cis.dtype==torch.complex64,"Precomputed cis tensor must have complex64 dtype"
+        self.register_buffer("freqs_cis", freqs_cis, persistent=False)
                     
         # init all weights
         self.apply(self._init_weights)
