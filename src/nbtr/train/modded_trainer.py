e99df20bd8aee57f57531a87a0ffe15ca91d7066
habanoz <huseyinabanox@gmail.com>
2024-10-31 13:45:18 +0300

    save error.

--- src/nbtr/train/modded_trainer.py
+++ src/nbtr/train/modded_trainer.py
@@ -433,7 +433,8 @@ class Trainer:
         
         new_val_loss = eval_out['val'].loss
         if  new_val_loss < self.state.best_val_loss:
-            self.state = TrainingState(iter_num=it, best_val_loss=new_val_loss, optim_state=optimizer.state_dict())
+            # self.state = TrainingState(iter_num=it, best_val_loss=new_val_loss, optim_state=optimizer.state_dict())
+            self.state = TrainingState(iter_num=it, best_val_loss=new_val_loss, optim_state=dict())
             
             self.on_new_best_val_loss(model)
                 
